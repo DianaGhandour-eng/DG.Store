4e32490a9775bc2d660941780e45eccd5c00783e
DianaGhandour-eng <eng.dianaghandour@gmail.com>
2023-07-28 04:32:36 +0300

    upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { ProductsModule } from './products/products.module';
+import { CartsModule } from './carts/carts.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    FormsModule,
+    ProductsModule,
+    CartsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
